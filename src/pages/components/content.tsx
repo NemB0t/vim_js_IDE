90b352d3658f564095dd7f53eccf55f494c24fdf
Nembot <adwaith1998kv@gmail.com>
2022-12-10 20:10:02 +0530

    Styling

--- src/pages/components/content.tsx
+++ src/pages/components/content.tsx
@@ -1,15 +1,15 @@
 import { CodeEditor } from "./codeEditor";
-import { Output } from "./output";
+// import { Output } from "./output";
 
 export function Content(){
     return(
-        <div className="h-full grid grid-cols-[2fr_1fr] grid-rows-2 items-center justify-items-center">
-            <div className="row-span-full"><CodeEditor/></div>
-            <div className="h-full w-full box-border p-4">
+        <div className="h-full grid grid-cols-[2fr_1fr] grid-rows-2 items-center justify-items-center gap-4 p-4 bg-zinc-300">
+            <div className="row-span-full bg-white h-full w-full rounded-md border border-black"><CodeEditor/></div>
+            <div className="h-full w-full box-border ">
                 <textarea className="w-full h-full border border-black rounded-md  box-border p-4 resize-none" placeholder="Input"/>
             </div>
-            <div className="h-full w-full box-border p-4">
-                <div className="w-full h-full border border-black rounded-md  box-border p-4 resize-none">
+            <div className="h-full w-full box-border">
+                <div className="w-full h-full border border-black rounded-md  box-border p-4 resize-none bg-white">
                     Waiting for the Output
                 </div>
             </div>
